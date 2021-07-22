59a27b4712d8749a50987e7c6953e77ed85f33d8
AlberVini <viiniciusalberto@gmail.com>
2021-07-21 22:30:36 -0300

    Update POO_2.py

--- codes/POO_2.py
+++ codes/POO_2.py
@@ -1,35 +1,55 @@
 from datetime import datetime
+from random import randint
+
 
 class Pessoa:
     ano_atual = int(datetime.strftime(datetime.now(), '%Y'))
-    def __init__(self, nome, ano_nasc, cidade):
+    def __init__(self, nome, idade, estado):
         self.nome = nome
-        self.ano_nasc = ano_nasc
-        self.cidade = cidade
-        self.lista_clientes = {'clientes': {}, 'idade': {}, 'cidade': {}}
+        self.idade = idade
+        self.estado = estado
 
-    def criacao_dic(self):
-        self.lista_clientes['clientes'] = self.nome
-        self.lista_clientes['idade'] = self.obter_idade()
-        self.lista_clientes['cidade'] = self.cidade
+    def ano_nascimento(self):
+        ano_nasc = Pessoa.ano_atual - self.idade
+        return f'{self.nome} nasceu no ano de {ano_nasc}'
 
-    def infos(self):
-        for key, item in self.lista_clientes.items():
-            print(self.lista_clientes)
-            print(key, item)
+    def info(self):
+        return f'{self.nome} tem {self.idade} anos e mora no Estado de {self.estado}'
+
+    # pode ser utilizado como um construtor, para por exemplo ajustar valores passados do usuário
+    @classmethod
+    def passagem_ano_nascimento(cls, nome, ano_nascimento, cidade):
+        idade = cls.ano_atual - ano_nascimento
+        return cls(nome, idade, cidade)
 
     @classmethod
-    def por_ano_nascimento(cls, ano_nasc):
-        idade = cls.ano_atual - ano_nasc
-        return idade
+    def ajuste_por_string(cls, pessoa_str):
+        nome, idade, estado = pessoa_str.split('-')
+        return cls(nome, int(idade), estado)
 
-    def obter_idade(self):
-        idade = self.por_ano_nascimento(self.ano_nasc)
-        return idade
+    # staticmethod é como uma função comum, portanto, pode ser usado de forma independente
+    @staticmethod
+    def gera_id():
+        num = randint(1000, 1999)
+        return num
 
 
-p1 = Pessoa('Vinicius', 1997, 'SP')
-p1.criacao_dic()
-p2 = Pessoa('Júlia', 1980, 'RJ')
-p2.criacao_dic()
-p2.infos()
+# passagem direta por classe
+p1 = Pessoa('Gustavo', 30, 'RJ')
+print(p1.info())
+
+# passagem por classmethod, adequando os valores passados por váriavel 
+pessoa_string = 'João-21-SP'
+p2 = Pessoa.ajuste_por_string(pessoa_string)
+print(p2.info())
+
+# passagem por classmethod, ajustando o valor do parametro idade passado como ano_nasc
+p3 = Pessoa.passagem_ano_nascimento('Maria', 1987, 'PR')
+print(p3.info())
+print(p3.ano_nascimento())
+
+p4 = Pessoa('Clara', 19, 'GO')
+id = p4.gera_id()
+print(f'{p4.info()} e tem o id = {id}')
+
+print(Pessoa.gera_id())
